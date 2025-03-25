9c3020411b313f3387079f4e78d2a4f4e80bc9fe
Shruti pandey <shrutipandey1505@gmail.com>
2025-03-25 20:40:34 +0530

    Test: Add test for single number input (fails as expected)

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,3 +3,8 @@
 from calculator.calculator import add
 def test_empty_string():
     assert add("") == 0 
+
+def test_single_number():
+    """Test that a single number returns itself."""
+    assert add("1") == 1
+    assert add("2") == 2
